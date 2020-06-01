2672bfa3e461200b55ff58fb51e6d817987a3b49
Quang Anh Hoang <qh62@drexel.edu>
2020-05-31 20:06:53 -0400

    Kernel check if tid is smaller than num_rows

--- A5_CUDA_Jacobi_Solver/jacobi_iteration_kernel.cu
+++ A5_CUDA_Jacobi_Solver/jacobi_iteration_kernel.cu
@@ -7,20 +7,22 @@ __global__ void jacobi_iteration_kernel_naive(const float *A, float *src, float 
 {
     int tid = blockIdx.x * blockDim.x + threadIdx.x;
     
-    int i;
-    float sum = 0.0;
-    for (i = 0; i < size; i++) {
-        if (i != tid) {
-            sum += A[tid*size + i] * src[i];
+    if (tid < size) {
+        int i;
+        float sum = 0.0;
+        for (i = 0; i < size; i++) {
+            if (i != tid) {
+                sum += A[tid*size + i] * src[i];
+            }
         }
+    
+        dest[tid] = (b[tid] - sum)/A[tid*size + tid];
+    
+        float local_diff = (dest[tid] - src[tid])*(dest[tid] - src[tid]);
+        __syncthreads();
+    
+        atomicAdd(ssd, local_diff);
     }
-
-    dest[tid] = (b[tid] - sum)/A[tid*size + tid];
-
-    float local_diff = (dest[tid] - src[tid])*(dest[tid] - src[tid]);
-    __syncthreads();
-
-    atomicAdd(ssd, local_diff);
 
     return;
 }
